5f2b609929c5da589634438c8f1b2d96e3045797
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-09-24 10:07:27 +0530

    Update g.py

--- g.py
+++ g.py
@@ -1,9 +1,15 @@
-#here i am added upto 2 accs which auto login and tweet if you want more means copy line from 57 to 96 and paste that in 97 line use note++ to find the number line if you want more means do copy paste and again and again and dont forgot to replace acc with password
-#(note for auto login used accs must be non authentication accounts)
-
-import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import time
 import random
 
 commentsDict = ['good','amazing one','keep going','excellent','next video please','sub to your channel','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
@@ -13,87 +19,86 @@ driver= webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("https://twitter.com/i/flow/login")
-time.sleep(11)
+time.sleep(7)
 email = driver.find_element_by_name('text')
-email.send_keys("@12q121") #replace with your twitter username
+email.send_keys("twitter123") #replace with your valid twitter username
 email.send_keys(Keys.ENTER)
 time.sleep(3)
 password = driver.find_element_by_name("password")
-password.send_keys("twitter12#$") #replace with your twitter password
+password.send_keys("twitter@#$%") #replace with your valid twitter password
 password.send_keys(Keys.ENTER)
-time.sleep(10)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
+time.sleep(5)
+
+
+counter = 0
+while True:
+    try:
+        
+        # Post the tweet
+        send_button = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
+        send_button.send_keys(random.choice(commentsDict))
+        
+        time.sleep(1)
+
+        # Click the post button
+        next_button = driver.find_element(By.XPATH, '//div[@data-testid="toolBar"]/div[2]/div[3]/div')
+        next_button.click()
+
+        time.sleep(2)  
+
+           
+        counter += 1
+        if counter == 5: #change how much you want tweets per acc
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
+time.sleep(2)
 driver.close()
 
 driver= webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("https://twitter.com/i/flow/login")
-time.sleep(11)
+time.sleep(7)
 email = driver.find_element_by_name('text')
-email.send_keys("@12wewrt") #replace with your twitter username
+email.send_keys("tweet123@#$%") #replace with your valid twitter username
 email.send_keys(Keys.ENTER)
 time.sleep(3)
 password = driver.find_element_by_name("password")
-password.send_keys("elonwes$%&") #replace with your twitter password
+password.send_keys("tweet123@#$%") #replace with your valid twitter password
 password.send_keys(Keys.ENTER)
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
-driver.find_element_by_class_name('public-DraftStyleDefault-block').send_keys(random.choice(commentsDict))
-driver.find_element_by_xpath('//div[@data-testid="toolBar"]/div[2]/div[3]/div').click()
-time.sleep(4)
+time.sleep(5)
+
+
+counter = 0
+while True:
+    try:
+        
+        # Post the tweet
+        send_button = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
+        send_button.send_keys(random.choice(commentsDict))
+        
+        time.sleep(1)
+
+        # Click the post button
+        next_button = driver.find_element(By.XPATH, '//div[@data-testid="toolBar"]/div[2]/div[3]/div')
+        next_button.click()
+
+        time.sleep(2)  
+
+           
+        counter += 1
+        if counter == 5: #change how much you want tweets per acc
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
+time.sleep(2)
 driver.close()
 
 
