3cc58caf0485b18e4b8d8bdc8b7658d648497d08
Yashwanth R <89150253+yashu1wwww@users.noreply.github.com>
2025-02-09 11:19:21 +0530

    Update g.py

--- g.py
+++ g.py
@@ -1,3 +1,4 @@
+#upto 5 accounts you can auto tweet
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
@@ -18,36 +19,33 @@ commentsDict = ['good','amazing one','keep going','excellent','next video please
 driver= webdriver.Chrome()
 driver.maximize_window()
 
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(7)
-email = driver.find_element_by_name('text')
-email.send_keys("tweet123") #replace with your valid twitter username
-email.send_keys(Keys.ENTER)
-time.sleep(3)
-password = driver.find_element_by_name("password")
-password.send_keys("tweet123@#$%") #replace with your valid twitter password
-password.send_keys(Keys.ENTER)
+driver.get("https://twitter.com/compose/tweet") #url redirect to login 
 time.sleep(5)
-
+email = driver.find_element_by_name('text')
+email.send_keys("Twitter123") #replace with your valid twitter username
+email.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys("Twitter_12345") #replace with your valid twitter password
+password.send_keys(Keys.ENTER)
+time.sleep(3)
 
 counter = 0
 while True:
     try:
         
-        # Post the tweet
-        send_button = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
+        #Post the tweet
+        send_button = driver.find_element(By.XPATH, '//div[@data-testid="tweetTextarea_0"]')
         send_button.send_keys(random.choice(commentsDict))
-        
         time.sleep(1)
 
-        # Click the post button
-        driver.execute_script('document.querySelector("#react-root > div > div > div.css-175oi2r.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-14lw9ot.r-jxzhtn.r-13l2t4g.r-1ljd8xs.r-1phboty.r-16y2uox.r-184en5c.r-61z16t.r-11wrixw.r-1jgb5lz.r-13qz1uu.r-1ye8kvj > div > div.css-175oi2r.r-14lw9ot.r-184en5c > div > div.css-175oi2r.r-14lw9ot.r-1h8ys4a > div:nth-child(1) > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-175oi2r.r-14lw9ot.r-jumn1c.r-xd6kpl.r-gtdqiz.r-ipm5af.r-184en5c > div:nth-child(2) > div > div > div > div.css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-19u6a5r.r-2yi16.r-1qi8awa.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l > div > span > span").click()')
-
+        #Click the post button
+        post_button = driver.find_element(By.XPATH, '//span[text()="Post"]')
+        post_button.click()
         time.sleep(2)  
 
-           
         counter += 1
-        if counter == 5: #Change '5' to 'how many auto tweet for a acc....
+        if counter == 5: #Change '5' to 'how many auto tweets for a acc....
             break
         
     except Exception as e:
@@ -57,40 +55,37 @@ while True:
 time.sleep(2)
 driver.close()
 
+#2nd account login
 driver= webdriver.Chrome()
 driver.maximize_window()
 
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(7)
-email = driver.find_element_by_name('text')
-email.send_keys("tweet123") #replace with your valid twitter username
-email.send_keys(Keys.ENTER)
-time.sleep(3)
-password = driver.find_element_by_name("password")
-password.send_keys("tweet123@#$%") #replace with your valid twitter password
-password.send_keys(Keys.ENTER)
+driver.get("https://twitter.com/compose/tweet") #url redirect to login 
 time.sleep(5)
-
+email = driver.find_element_by_name('text')
+email.send_keys("Twitter123") #replace with your valid twitter username
+email.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys("Twitter_12345") #replace with your valid twitter password
+password.send_keys(Keys.ENTER)
+time.sleep(3)
 
 counter = 0
 while True:
     try:
         
-        # Post the tweet
-        send_button = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
+        #Post the tweet
+        send_button = driver.find_element(By.XPATH, '//div[@data-testid="tweetTextarea_0"]')
         send_button.send_keys(random.choice(commentsDict))
-        
         time.sleep(1)
 
-        # Click the post button
-        driver.execute_script('document.querySelector("#react-root > div > div > div.css-175oi2r.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-14lw9ot.r-jxzhtn.r-13l2t4g.r-1ljd8xs.r-1phboty.r-16y2uox.r-184en5c.r-61z16t.r-11wrixw.r-1jgb5lz.r-13qz1uu.r-1ye8kvj > div > div.css-175oi2r.r-14lw9ot.r-184en5c > div > div.css-175oi2r.r-14lw9ot.r-1h8ys4a > div:nth-child(1) > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-175oi2r.r-14lw9ot.r-jumn1c.r-xd6kpl.r-gtdqiz.r-ipm5af.r-184en5c > div:nth-child(2) > div > div > div > div.css-175oi2r.r-sdzlij.r-1phboty.r-rs99b7.r-lrvibr.r-19u6a5r.r-2yi16.r-1qi8awa.r-ymttw5.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l > div > span > span").click()')
-
-
+        #Click the post button
+        post_button = driver.find_element(By.XPATH, '//span[text()="Post"]')
+        post_button.click()
         time.sleep(2)  
 
-           
         counter += 1
-        if counter == 5: #Change '5' to 'how many auto tweet for a acc....
+        if counter == 5: #Change '5' to 'how many auto tweets for a acc....
             break
         
     except Exception as e:
@@ -99,4 +94,126 @@ while True:
         
 time.sleep(2)
 driver.close()
+
+#3rd account login
+driver= webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://twitter.com/compose/tweet") #url redirect to login 
+time.sleep(5)
+email = driver.find_element_by_name('text')
+email.send_keys("Twitter123") #replace with your valid twitter username
+email.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys("Twitter_12345") #replace with your valid twitter password
+password.send_keys(Keys.ENTER)
+time.sleep(3)
+
+counter = 0
+while True:
+    try:
+        
+        #Post the tweet
+        send_button = driver.find_element(By.XPATH, '//div[@data-testid="tweetTextarea_0"]')
+        send_button.send_keys(random.choice(commentsDict))
+        time.sleep(1)
+
+        #Click the post button
+        post_button = driver.find_element(By.XPATH, '//span[text()="Post"]')
+        post_button.click()
+        time.sleep(2)  
+
+        counter += 1
+        if counter == 5: #Change '5' to 'how many auto tweets for a acc....
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
+time.sleep(2)
+driver.close()
+
+#4th account login
+driver= webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://twitter.com/compose/tweet") #url redirect to login 
+time.sleep(5)
+email = driver.find_element_by_name('text')
+email.send_keys("Twitter123") #replace with your valid twitter username
+email.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys("Twitter_12345") #replace with your valid twitter password
+password.send_keys(Keys.ENTER)
+time.sleep(3)
+
+counter = 0
+while True:
+    try:
+        
+        #Post the tweet
+        send_button = driver.find_element(By.XPATH, '//div[@data-testid="tweetTextarea_0"]')
+        send_button.send_keys(random.choice(commentsDict))
+        time.sleep(1)
+
+        #Click the post button
+        post_button = driver.find_element(By.XPATH, '//span[text()="Post"]')
+        post_button.click()
+        time.sleep(2)  
+
+        counter += 1
+        if counter == 5: #Change '5' to 'how many auto tweets for a acc....
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
+time.sleep(2)
+driver.close()
+
+#5th account login
+driver= webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://twitter.com/compose/tweet") #url redirect to login 
+time.sleep(5)
+email = driver.find_element_by_name('text')
+email.send_keys("Twitter123") #replace with your valid twitter username
+email.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name("password")
+password.send_keys("Twitter_12345") #replace with your valid twitter password
+password.send_keys(Keys.ENTER)
+time.sleep(3)
+
+counter = 0
+while True:
+    try:
+        
+        #Post the tweet
+        send_button = driver.find_element(By.XPATH, '//div[@data-testid="tweetTextarea_0"]')
+        send_button.send_keys(random.choice(commentsDict))
+        time.sleep(1)
+
+        #Click the post button
+        post_button = driver.find_element(By.XPATH, '//span[text()="Post"]')
+        post_button.click()
+        time.sleep(2)  
+
+        counter += 1
+        if counter == 5: #Change '5' to 'how many auto tweets for a acc....
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
+time.sleep(2)
+driver.close()
+
+
 
